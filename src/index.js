4f1469536fa4cb32ecfe717b3f036557a7461c63
Egor Filipas <filipasegor@gmail.com>
2021-03-08 17:34:59 +0300

    first

--- src/index.js
+++ src/index.js
@@ -1,17 +1,65 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles.css';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import { useState, useEffect, useLayoutEffect } from "react";
+import { render } from "react-dom";
+import mapboxgl from "mapbox-gl";
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function App() {
+  const [random, setRandom] = useState(Math.random());
+  const [coordinates, setCoordinates] = useState();
+  const [lngLat, setLngLat] = useState([]);
+  const [marker, setMarker] = useState(undefined);
+
+  mapboxgl.accessToken =
+    "pk.eyJ1IjoiZmlsaXBhcyIsImEiOiJja2x4dTVoZ2kweHAxMndvNjZ2bjVxcThmIn0.RDd66wk54EmYS4TcXnRPIg";
+
+    useLayoutEffect(() => {
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/dark-v10',
+      center: [55.7558, 37.6173],
+      zoom: 10
+    })
+
+
+    const market = new mapboxgl.Marker()
+    .setLngLat([55.7558, 37.6173])
+    .addTo(map)
+
+    setMarker(new mapboxgl.Marker);
+
+  }, [])
+
+
+  const stores = {
+    km20: [55.761994, 37.610641],
+    belief: [55.733396, 37.601152],
+    brandshop: [55.767839, 37.616812]
+  };
+
+  return (
+    <>
+
+      <button id="rerender" onClick={() => setRandom(Math.random())}>
+        Ререндер!
+      </button>
+      <div style={{height:"800px"}} id="map"></div>
+
+      <div className="map-overlay">
+        <h3>Выберите магазин: </h3>
+        <select onChange={e => setMarker(e.target.setLngLat[10, 20])} >
+          <option value="km20" >KM20</option>
+          <option value="belief">BELIEF</option>
+          <option value="brandshop">BRANDSHOP</option>
+        </select>
+      </div>
+      <div id="map"></div>
+
+    </>
+  );
+}
+
+render(<App />, document.querySelector("#root"));
